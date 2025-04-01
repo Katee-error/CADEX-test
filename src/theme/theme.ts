6fa47788a0e6a0351b06ffe33194f27facd77071
Ekaterina <kate8190758@gmail.com>
2025-04-01 15:55:53 +0400

    feat: add chakra theme provider

---
+++ src/theme/theme.ts
@@ -0,0 +1,46 @@
+"use client";
+
+import { extendTheme, ThemeConfig, withDefaultColorScheme } from "@chakra-ui/react";
+
+const semanticColors = {
+  primaryText: "#fafafa",
+  secondaryText: "rgba(255, 255, 255, 0.6)",
+  headingSecondaryText: "rgba(255, 255, 255, 0.7)",
+  background: "white",
+  accent: "#0F89D3",
+};
+
+const config: ThemeConfig = {
+  initialColorMode: "light",
+  useSystemColorMode: true,
+};
+
+export const theme = extendTheme(
+  withDefaultColorScheme({ colorScheme: config.initialColorMode }),
+  {
+    styles: {
+      global: {
+        body: {
+          backgroundColor: "background",
+          height: "100vh",
+        },
+      },
+    },
+    colors: {
+      ...semanticColors,
+    },
+    components: {
+      Badge: {
+        baseStyle: {
+          color: "primaryText",
+          bgColor: "accent",
+          padding: "5px 10px",
+          mb: "20px",
+          borderRadius: "full",
+          fontWeight: 600,
+          fontSize: "14px",
+        },
+      },
+    },
+  }
+);
